996b09e9b9c789ee962c41de62ccec37b83718f5
Julio <jn5.guape@gmail.com>
2021-06-18 11:07:21 -0300

    black

--- core/migrations/0009_olimpimat_regulamento_alterado.py
+++ core/migrations/0009_olimpimat_regulamento_alterado.py
@@ -6,14 +6,14 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0008_auto_20210614_1358'),
+        ("core", "0008_auto_20210614_1358"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='olimpimat',
-            name='regulamento_alterado',
-            field=models.FileField(default='', upload_to='regulamento/%Y/%m/%d/'),
+            model_name="olimpimat",
+            name="regulamento_alterado",
+            field=models.FileField(default="", upload_to="regulamento/%Y/%m/%d/"),
             preserve_default=False,
         ),
     ]
